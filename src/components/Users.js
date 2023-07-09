cd412a47d984ad2b8c443e2b02779802b0efa03b
RolandOroz <rolandoroz@gmail.com>
2023-07-09 23:21:14 +0100

    .

--- src/components/Users.js
+++ src/components/Users.js
@@ -4,6 +4,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 
 const Users = () => {
   const [username,setUsers] = useState();
+  const [userEmail,setUserEmail] = useState();
   const axiosPrivate = useAxiosPrivate();
   const navigate = useNavigate();
   const location = useLocation();
@@ -15,39 +16,41 @@ const Users = () => {
 
     const getUsers = async () => {
       try {
-          const response = await axiosPrivate.get("/users", {
-            signal: controller.abort()
-          });
-          const userNames = response.data.map(user => user.username);
-          //TODO delete log
-          console.log(response.data);
-          //isMounted && setUsers(response.data);
-          isMounted && setUsers(userNames);
+        const response = await axiosPrivate.get("/users", {
+          signal: controller.abort(),
+        });
+        const userNames = response.data.map((user) => user.username);
+        const userEmail = response.data.map((user) => user.email);
+        //TODO delete log
+        console.log(response.data);
+        //isMounted && setUsers(response.data);
+        isMounted && setUsers(userNames) && setUserEmail(userEmail);
       } catch (error) {
-          console.error(error);
-          navigate("/login", { state: { from: location}, replace: true});
+        console.error(error);
+        navigate("/login", { state: { from: location }, replace: true });
       }
-    }
+    };
 
     getUsers();
 
     return () => {
-       isMounted = false;
+      isMounted = false;
       //controller.abort();
       isMounted && controller.abort();
       //effectRun.current = true;
-    }
-  })
+    };
+  }, [axiosPrivate, location, navigate]);
 
   return (
     <article>
       <h2>Users List</h2>
       {username?.length ? (
         <ul>
-          {username.map((user, i) => (
+          {username.map((userN, i) => (
             //{/* <li key={i}>{user?.username}</li> */}
-            <li key={i}>{user}</li>
+            <li key={i}>{userN}</li>
           ))}
+
         </ul>
       ) : (
         <p>No User to Display</p>
